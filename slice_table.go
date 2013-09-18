607e2cdd17c303e31aee17ef53406afa973c992e
hearts <hearts.zhang@gmail.com>
2013-09-18 18:02:45 +0800

    make unit test passed
    - rename rollback to reset_unready
    - rename online_fd to file_desc
    - fix update(index,.)
    - fix unready_after returns wrong range

--- slice_table.go
+++ slice_table.go
@@ -1,54 +1,62 @@
 package onlinefile
 
+import ()
+
+import ()
+
 const (
 	slice_entry_bits byte = 2 // 2 bits
 	slice_entry_mask byte = 1<<slice_entry_bits - 1
-
+)
+const (
 	slice_status_unready slice_entry = iota
 	slice_status_pending
 	slice_status_ready
+
+	byte_bits = 8
 )
 
-type slice_entry byte
+type slice_entry byte // 00 00 00 xx , only the lowest two bits be used
+
 type slice_table interface {
 	entry(index int) slice_entry
 	update(index int, status slice_entry) slice_entry
-	unready_after(startidx, max_count int) (beginidx, endidx int)
-	rollback(beginidx, endidx int)
+	unready_after(startidx, max_count int) (beginidx, endidx int) // will update [begin,end) to pending
+	reset_unready(beginidx, endidx int)                           // reset [begin,end) to unready
 	mark_ready(beginidx, endidx int)
-	avail_after(index int) (count int)
-	resize(length, slice_size int64) // len is content-length not the slice-table length
+	avail_after(index int) (count int) //continuous slices from [index...
+	resize(length, slice_size int64)   // len is content-length not the slice-table length
 }
 
-type online_fd []byte
+type file_desc []byte // implements interface slice_table
 
-func new_online_fd(length, slice_size int64) slice_table {
-	of := make(online_fd, 0)
+func new_file_desc(length, slice_size int64) slice_table {
+	of := make(file_desc, 0)
 	of.resize(length, slice_size)
 	return &of
 }
 
-func (this *online_fd) resize(length, slice_size int64) {
+func (this *file_desc) resize(length, slice_size int64) {
 	slice_count := (length + slice_size - 1) / slice_size
-	data_length := int(slice_count+7) * int(slice_entry_bits) / 8
+	data_length := int(slice_count+byte_bits-1) / byte_bits * int(slice_entry_bits)
 
 	if data_length == len(*this) {
 		return
 	}
-	v := make(online_fd, data_length)
+	v := make(file_desc, data_length)
 	copy(v, *this)
 	*this = v
 }
 
-func (this *online_fd) entry(index int) slice_entry {
-	c := 8 / int(slice_entry_bits)
+func (this *file_desc) entry(index int) slice_entry {
+	c := byte_bits / int(slice_entry_bits)
 	byte_idx := index / c
-	in_byte := byte(index % int(c))
-	v := ((*this)[byte_idx] >> (in_byte * slice_entry_bits)) & slice_entry_mask
+	in_byte := byte(index%int(c)) * slice_entry_bits
+	v := ((*this)[byte_idx] >> in_byte) & slice_entry_mask
 	return slice_entry(v)
 }
 
-func (this *online_fd) avail_after(index int) (count int) {
+func (this *file_desc) avail_after(index int) (count int) {
 	for end := len(*this); index < end; index++ {
 		s := this.entry(index)
 		if s != slice_status_ready {
@@ -59,20 +67,26 @@ func (this *online_fd) avail_after(index int) (count int) {
 	return
 }
 
-func (this *online_fd) update(index int, status slice_entry) slice_entry {
-	c := 8 / int(slice_entry_bits)
+func (this *file_desc) update(index int, status slice_entry) slice_entry {
+	c := byte_bits / int(slice_entry_bits)
 	bidx := index / c
 	in_byte := index % c
 	v := (*this)[bidx]
 	l := status << (byte(in_byte) * slice_entry_bits)
 	mask := ^(slice_entry_mask << (byte(in_byte) * slice_entry_bits))
 	v = v&mask | byte(l)
-	return slice_entry(v)
+	(*this)[bidx] = v
+	return status
 }
-
-func (this *online_fd) unready_after(start, count int) (begin, end int) {
-	begin = start + count     // bint
-	c := 8 / slice_entry_bits // byte
+func mini(lhs, rhs int) int {
+	if lhs < rhs {
+		return lhs
+	}
+	return rhs
+}
+func (this *file_desc) unready_after(start, count int) (begin, end int) {
+	begin = start + count             // bint
+	c := byte_bits / slice_entry_bits // byte
 	for i := start; i < start+count; i++ {
 		in_byte := i % int(c)
 		mask := slice_entry_mask << (slice_entry_bits * byte(in_byte))
@@ -82,8 +96,9 @@ func (this *online_fd) unready_after(start, count int) (begin, end int) {
 			break
 		}
 	}
-	end = start + count
-	for i := begin; i < start+count; i++ {
+	end = begin + count
+	b := mini(begin+count, len(*this))
+	for i := begin; i < b; i++ {
 		in_byte := i % int(c)
 		mask := slice_entry_mask << (slice_entry_bits * byte(in_byte))
 		idx := i / int(c)
@@ -98,13 +113,13 @@ func (this *online_fd) unready_after(start, count int) (begin, end int) {
 	return
 }
 
-func (this *online_fd) rollback(begin, end int) {
+func (this *file_desc) reset_unready(begin, end int) {
 	for i := begin; i < end; i++ {
 		this.update(i, slice_status_unready)
 	}
 }
 
-func (this *online_fd) mark_ready(begin, end int) {
+func (this *file_desc) mark_ready(begin, end int) {
 	for i := begin; i < end; i++ {
 		this.update(i, slice_status_ready)
 	}
